966026b1b2bff58782ea8087abb9420798af53bf
Jonas Halstrup <jhalstrup@googlemail.com>
2017-03-29 18:27:39 +0200

    implemented kmeans - doesn't seem to work properly

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,29 +1,26 @@
 import regex as re
+import numpy as np
+from sklearn.cluster import KMeans
+
 
 f = open("CoinToss.txt", "r")
 lines = []
 for line in f:
     lines += [line]
 
-#print(len(lines[1]), "\n")
-ones = []
-for i, line in enumerate(lines):
-    ones += [(len(''.join(c for c in line if c == '1')), i)]
-
-#Assume 50/50 split of 0s/1s ~ sort for greatest diviation 
-ones_ = list(map(lambda x: (abs(100-x[0]), x[1]), ones))
-ones_sorted = sorted(ones_)
-print(ones_sorted)
+print(len(lines[1]), "\n")
 
 def subseq_1_freq(line):
-    total_1 = len(line)
-    _0 = len(re.findall(r'0', line)) / total
-    _1 = len(re.findall(r'1', line)) / total
+    # \n at the end
+    total_1 = len(line) -1
+    _0 = len(re.findall(r'0', line)) / total_1
+    _1 = len(re.findall(r'1', line)) / total_1
 
     return (_0, _1)
 
 def subseq_2_freq(line):
-    total_2 = len(line) - 2
+    #total number or pairs
+    total_2 = len(line) - 2 -1
     
     _00 = len(re.findall(r'00', line, overlapped=True)) / total_2
     _01 = len(re.findall(r'01', line, overlapped=True)) / total_2
@@ -33,7 +30,8 @@ def subseq_2_freq(line):
     return (_00, _01, _10, _11)
     
 def subseq_3_freq(line):
-    total_3 = len(line) - 3
+    #because \n at the end
+    total_3 = len(line) - 3 -1 
     
     _000 = len(re.findall(r'000', line, overlapped=True)) / total_3
     _001 = len(re.findall(r'001', line, overlapped=True)) / total_3
@@ -46,29 +44,61 @@ def subseq_3_freq(line):
 
     return (_000, _001, _010, _011, _100, _101, _110, _111)
 
-def to_2_err(tlp):
-    return abs(tlp[0]-0.25) + abs(tlp[1]-0.25) + \
-        abs(tlp[2]-0.25) + abs(tlp[3]-0.25) 
+def to_1_err(tpl):
+    return abs(tpl[0]-0.5) + abs(tpl[1]-0.5)
 
-def to_3_err(tlp):
-    return abs(tlp[0]-0.125) + abs(tlp[1]-0.125) + abs(tlp[2]-0.125) + \
-        abs(tlp[3]-0.125) + abs(tlp[4]-0.125) + abs(tlp[5]-0.125) + \
-        abs(tlp[6]-0.125) + abs(tlp[7]-0.125)
+def to_2_err(tpl):
+    return abs(tpl[0]-0.25) + abs(tpl[1]-0.25) + \
+        abs(tpl[2]-0.25) + abs(tpl[3]-0.25) 
 
+def to_3_err(tpl):
+    return abs(tpl[0]-0.125) + abs(tpl[1]-0.125) + abs(tpl[2]-0.125) + \
+        abs(tpl[3]-0.125) + abs(tpl[4]-0.125) + abs(tpl[5]-0.125) + \
+        abs(tpl[6]-0.125) + abs(tpl[7]-0.125)
+
+def max_1_err(tpl):
+    return abs(tpl[0])-0.5
+
+def max_2_err(tpl):
+    return max(abs(tpl[0]-0.25), abs(tpl[1]-0.25), abs(tpl[2]-0.25), \
+               abs(tpl[3]-0.25))
+
+def max_3_err(tpl):
+    return max(abs(tpl[0]-0.125), abs(tpl[1]-0.125), abs(tpl[2]-0.125), \
+               abs(tpl[3]-0.125), abs(tpl[4]-0.125), abs(tpl[3]-0.125), \
+               abs(tpl[6]-0.125), abs(tpl[7]-0.125))
+
+freq_1 = []
 freq_2 = []
 freq_3 = []
 for i, line in enumerate(lines):
+    freq_1 += [(subseq_1_freq(line), i)]
     freq_2 += [(subseq_2_freq(line), i)]
     freq_3 += [(subseq_3_freq(line), i)]
 
-freq_2 = sorted(freq_2)
-freq_3 = sorted(freq_3)
+#freq_1 = sorted(freq_1)
+#freq_2 = sorted(freq_2)
+#freq_3 = sorted(freq_3)
 
-#print(freq_2)
-#print(freq_3)
+err_1_list = (list(map(lambda x : (to_1_err(x[0]), x[1]), freq_1)))
+err_2_list = (list(map(lambda x : (to_2_err(x[0]), x[1]), freq_2)))
+err_3_list = (list(map(lambda x : (to_3_err(x[0]), x[1]), freq_3)))
 
-err_2_list = sorted(list(map(lambda x : (to_2_err(x[0]), x[1]), freq_2)))
-err_3_list = sorted(list(map(lambda x : (to_3_err(x[0]), x[1]), freq_3)))
+#Remove remove index from tuple, gen numpy array, reshape it so that it can be used for kmeans
+pure_err_array_1 = np.array(list(map(lambda x: x[0],err_1_list))).reshape(-1,1)
+pure_err_array_2 = np.array(list(map(lambda x: x[0],err_2_list))).reshape(-1,1)
+pure_err_array_3 = np.array(list(map(lambda x: x[0],err_3_list))).reshape(-1,1)
 
-print("Err_2:\n", err_2_list)
-print("Err_3:\n", err_3_list)
+km = KMeans(n_clusters=2, max_iter=3000)
+km_lab_1 = km.fit(pure_err_array_1).labels_
+km_lab_2 = km.fit(pure_err_array_2).labels_
+km_lab_3 = km.fit(pure_err_array_3).labels_
+
+#Dummy output to check if the previous operations are correct
+print("Err_1:\n", err_1_list)
+print("P Err:\n", pure_err_array_1)
+#print("Err_2:\n", err_2_list)
+#print("Err_3:\n", err_3_list)
+print("\n\nKMeans results for 1-3 len subsequence freq. err.\n1:\n", km_lab_1)
+print("2:\n", km_lab_2)
+print("3:\n", km_lab_3)
