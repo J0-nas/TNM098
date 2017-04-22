2986a526d6972a874a9655f987b47931709fba2c
Alice Reinaudo <alire454@student.liu.se>
2017-04-22 15:02:20 +0200

    updated lum

--- Lab3/Lab3.1/luminance.py
+++ Lab3/Lab3.1/luminance.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+#def avg_rbg_zone()
+
 # One file per image with all the data
 for imgn in range(1,13):
 	if imgn < 10:
@@ -21,12 +23,12 @@ for imgn in range(1,13):
 	print(avg)
 	avg = np.divide(avg, (len(img) * len(img[0])))
 	avg_lumi += 0.2126 * avg[0] + 0.7152 * avg[1] + 0.0722 * avg[2]
-	#avg_lumi = np.divide(avg_lumi, (len(img) * len(img[0])))
 	print(avg)
 	print(avg_lumi)
-	data_file.write("# RGB (average, whole image)\n")
-	data_file.write("R:" + str(avg[0]) + " G:" + str(avg[1]) + " B:" + str(avg[2]) + "\n")
-	data_file.write("# Luminance (average, whole image)\n")
+	#data_file.write("# RGB (average, whole image)\n")
+	#data_file.write("R:" + str(avg[0]) + " G:" + str(avg[1]) + " B:" + str(avg[2]) + "\n")
+	data_file.write(str(avg[0]) + "," + str(avg[1]) + "," + str(avg[2]) + "\n")
+	#data_file.write("# Luminance (average, whole image)\n")
 	data_file.write(str(avg_lumi))
 
 # Luminance
