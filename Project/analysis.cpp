6baeaef065dc4e3f203ffd58807d9076fbbe9ece
Jonas Halstrup <jonas.halstrup@tu-harburg.de>
2017-05-03 00:56:38 +0200

    improvements...

--- Project/analysis.cpp
+++ Project/analysis.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <map>
 #include <iterator>
 #include <fstream>
 #include <iostream>
@@ -6,7 +7,7 @@
 #include <sstream>
 #include <iomanip>
 #include <ctime>
-
+#include <utility>
 
 using namespace std;
 
@@ -21,6 +22,27 @@ istream& operator>>(istream& is, WordDelimitedByComma& output)
 
 class DataPoint {
 public:
+  DataPoint(DataPoint& other) {
+    time_stamp = other.time_stamp;
+    car_id = other.car_id;
+    car_type = other.car_type;
+    checkpoint_id = other.checkpoint_id;
+    cout << "copy constructor" << endl;
+  }
+
+  DataPoint(DataPoint&& other) {
+    time_stamp = other.time_stamp;
+    car_id = other.car_id;
+    car_type = other.car_type;
+    checkpoint_id = other.checkpoint_id;
+    
+    //cout << "in move constructor" << endl;
+    other.time_stamp = -1;
+    other.car_id = "";
+    other.car_type = -1;
+    other.checkpoint_id = "";
+  }
+  
   DataPoint(string csv_line) {
     istringstream iss(csv_line);
     vector<string> words {istream_iterator<WordDelimitedByComma>{iss}, istream_iterator<WordDelimitedByComma>{}};
@@ -41,16 +63,22 @@ public:
     car_id = words[1];
     car_type = c_t;
     checkpoint_id = words[3];
+    //cout << "string constructor" << endl;
   }
 
   DataPoint(int t_s, string c_i, int c_t, string check_i):
     time_stamp {t_s}, car_id{c_i}, car_type {c_t}, checkpoint_id {check_i}
   {
-    cout << time_stamp << endl;
+    //cout << time_stamp << endl;
+    cout << "regular constructor" << endl;
   }
 
   string getSummary() {
     return "t_s: " + to_string(time_stamp) + " c_i: " + car_id + " c_t: " + to_string(car_type) + " c_i: " + checkpoint_id;
+  }
+
+  string getIdentifier() {
+    return string{car_id};
   }
   
 private:
@@ -66,12 +94,51 @@ int main(int argc, char** argv) {
   getline(ifs, line);
   cout << line << endl;
   vector<DataPoint> points {};
-  while(getline(ifs, line)) {
-    DataPoint tmp {line};
-    points.push_back(DataPoint{line});
+  int i = 0;
+  while(getline(ifs, line) && i < 30) {
+    points.push_back(move(DataPoint{line}));
+    i ++;
+  }
+  cout << "\tvector built" << endl;
+  
+  //cout << points.at(0).getSummary() << endl;
+  //cout << points.back().getSummary() << endl;
+  
+  /*
+  DataPoint a{2016, "i_a", 5, "check_a"};
+  DataPoint b{2016, "i_b", 6, "check_b"};
+  cout << a.getSummary() << endl;
+  cout << b.getSummary() << endl;
+  auto c = DataPoint{a};
+  cout << a.getSummary() << endl;
+  cout << c.getSummary() << endl;
+  auto d = move(a);
+  cout << a.getSummary() << endl;
+  cout << d.getSummary() << endl;
+  cout << "-----------" << endl;
+  map<string, vector<DataPoint>> m {};
+  //m["i"] = vector<DataPoint>{};
+  //m["i"].push_back(DataPoint{move(d)});
+  //cout << d.getSummary() << endl;
+  */
+  map<string, vector<DataPoint>> m {};
+  for (auto & i: points) {
+    if (m.find(i.getIdentifier()) == m.end()) {
+      m[i.getIdentifier()] = vector<DataPoint>{};
+    }
+    m[i.getIdentifier()].push_back(move(i));
+  }
+  cout << "\tmap built" << endl;
+
+  for (auto & n: m) {
+    for (auto & i: n.second) {
+      //cout << i.getSummary() << endl;
+    }
   }
 
-  
-  cout << points.at(0).getSummary() << endl;
-  cout << points.back().getSummary() << endl;
+  cout << "points" << endl;
+  for (auto & d: points) {
+    cout << d.getSummary() << endl;
+  }
+  //copy(m.begin(), m.end(), ostream_iterator<vector<DataPoint>>{cout});
 }
